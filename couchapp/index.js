ef67285069c18818d3db053399b06b748ea35f84
Max Thayer <garbados@gmail.com>
2014-03-06 19:41:56 -0500

    first pass at couchapp config

--- couchapp/index.js
+++ couchapp/index.js
@@ -1,55 +1,15 @@
 var couchapp = require('couchapp');
+var path = require('path');
 
 var ddoc = {
   _id: '_design/docscloudantcom',
   rewrites: require('./rewrites.json'),
-  views: {
-    format: {
-      map: function(doc){
-        if(doc.text && !doc.deleted && doc._id.indexOf('api/inc') === -1){
-          var title_pattern = /^(?:# )?(.+)\n/
-            , title = doc.text.match(title_pattern)
-            , id = doc._id.replace('index.md', '').replace(/\.\w{2,3}$/, '');
-          log(id);
-          if(title){
-            emit(id, {
-              title: title[1],
-              text: doc.text
-            }); 
-          }
-        }
-      }
-    }
-  },
-  lists: {
-    tree: function(head, req){
-      var tree = {},
-          row,
-          parts,
-          i,
-          temp;
-      while(row = getRow()){
-        parts = row.id.split('/');
-        temp = tree;
-        for(i in parts.slice(0, -1)){
-          temp[parts[i]] = temp[parts[i]] || {};
-          temp = temp[parts[i]];
-        }
-        temp[parts[parts.length - 1]] = row;
-      }
-      send(JSON.stringify(tree));
-    }
-  },
+  views: require('./views'),
+  indexes: require('./indexes')
+  lists: {},
   shows: {},
-  indexes: {
-    text: {
-      index: function(doc){
-        if(doc.text && !doc.deleted && doc._id.indexOf('api/inc') === -1){
-          index("default", doc.text);
-        }
-      }
-    }
-  }
 };
+
+couchapp.loadAttachments(ddoc, path.join(process.cwd(), 'dist'));
 
 module.exports = ddoc;
