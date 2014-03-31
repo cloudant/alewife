44677a732bfd879f2aed87dd68694b9c372b829f
Max Thayer <garbados@gmail.com>
2014-03-31 17:35:27 -0400

    scaffold now uses yaml :D

--- tasks/scaffold.js
+++ tasks/scaffold.js
@@ -2,114 +2,164 @@ var fs = require('fs');
 var path = require('path');
 var async = require('async');
 
-function scaffold (filepath, sitemap, done) {
-  function write_title (filepath, done) {
-    var filename = path.join(filepath, 'index.md');
-    async.series([
-      function (done) {
-        fs.mkdir(filepath, function (err) {
-          if (err && err.code !== 'EEXIST') {
-            done(err);
-          } else {
-            done();
-          }
-        });
-      },
-      function (done) {
-        fs.stat(filename, function (err, stat) {
-          if (!err) {
-            // file exists, skip writing
-            done(true);
-          } else if (err.code === 'ENOENT') {
-            // file doesn't exist, write it
-            done();
-          } else {
-            // some other error
-            done(err);
-          }
-        })
-      },
-      fs.writeFile.bind(fs, filename, 'TODO')
-    ], function (err) {
-      if (err && err !== true) {
-        done(err);
-      } else {
-        done();
-      }
-    });
-  }
-
-  function write_page (filepath, done) {
-    var filename = filepath + '.md';
-    async.series([
-      function (done) {
-        fs.stat(filename, function (err, stat) {
-          if (!err) {
-            // file exists, skip writing
-            done(true);
-          } else if (err.code === 'ENOENT') {
-            // file doesn't exist, write it
-            done();
-          } else {
-            // some other error
-            done(err);
-          }
-        })
-      },
-      fs.writeFile.bind(fs, filename, 'TODO')
-    ], function (err) {
-      if (err && err !== true) {
-        done(err);
-      } else {
-        done();
-      }
-    });
-  }
-
-  if (sitemap[0].forEach) {
-    // first element is an array
-    // break it down >:D
-    async.map(sitemap, scaffold.bind(null, filepath), done);
-  } else {
-    var tasks = [];
-
-    // first element is a string
-    // treat it as the title page
-    var title = sitemap[0];
-    filepath = path.join(filepath, title);
-    var pages = sitemap.slice(1); 
-
-    // write the title
-    tasks.push(write_title.bind(null, filepath));
-    
-    // write the pages
-    if (!pages.length) {
-      // nothing to do; no pages
-    } else if (pages[0].forEach) {
-      // pages are an array of arrays
-      // break it up!
-      tasks = tasks.concat(pages.map(function (page) {
-        return scaffold.bind(null, filepath, page);
-      }));
-    } else {
-      // it's an array of strings
-      // write them to disk
-      tasks = tasks.concat(pages.map(function (page) {
-        return write_page.bind(null, path.join(filepath, page));
-      }));
-    }
-
-    // resolve tasks
-    async.series(tasks, done);
-  }
+function Scaffold (options) {
+  this.filepath = options.filepath || 'docs';
+  this.sitemap = require(options.sitemap_path || '../config.json').sitemap;
 }
 
-module.exports = function (grunt) {
-  grunt.registerMultiTask('scaffold', 'Scaffold documentation with TODOs', function () {
-    var filepath = path.join(process.cwd(), this.data.folder);
-    var sitemap = this.data.sitemap;
-    var done = this.async();
+Scaffold.prototype.write = parse_sitemap;
+Scaffold.prototype.title = write_title;
+Scaffold.prototype.page = write_page;
 
-    scaffold(filepath, sitemap, done);
+function parse_sitemap (filepath, sitemap, done) {
+  // defaults
+  if (!done) {
+    done = sitemap || filepath;
+    
+    if (typeof(filepath) === 'object') {
+      sitemap = filepath;
+    } else {
+      sitemap = this.sitemap;
+    }
+
+    filepath = this.filepath;
+  }
+
+  // provide descendents a sense of selfhood
+  var self = this || {
+    sitemap: sitemap,
+    filepath: filepath
+  };
+
+  // handle different section type cases
+  // adding them to the tasks array
+  var tasks = [];
+  var filename;
+  sitemap.forEach(function (section) {
+    if (typeof(section) === 'string') {
+      // section is a string: write a page
+      filename = path.join(filepath, section);
+      tasks.push(write_page.bind(self, filename));
+    } else if (section instanceof Array) {
+      // section is an array: recurse parse_sitemap
+      tasks.push(parse_sitemap.bind(self, filepath, section));
+    } else if (typeof(section) === 'object') {
+      // section is an object: write titles for each key 
+      // and recuse parse_sitemap on value
+      Object.keys(section).forEach(function (key) {
+        filename = path.join(filepath, key);
+        if (section[key]) {
+          tasks.unshift(parse_sitemap.bind(self, filename, section[key])); 
+        }
+        tasks.unshift(write_title.bind(self, filename));
+      });
+    } else {
+      // else, throw a fit 
+      done(section);
+    }
   });
-};
+
+  async.series(tasks, done);
+}
+
+function write_title (filepath, done) {
+  // defaults
+  if (!done) {
+    done = filepath;
+    filepath = this.filepath;
+  }
+
+  // create the path to the title file itself
+  var filename = path.join(filepath, 'index.md');
+  async.series([
+    // create the containing folder
+    function (done) {
+      fs.mkdir(filepath, function (err) {
+        if (err && err.code !== 'EEXIST') {
+          done(err);
+        } else {
+          done();
+        }
+      });
+    },
+    // see if the file exists, and thus
+    // whether we should create it
+    fs.exists.bind(fs, filename),
+    // create the file if we got this far
+    fs.writeFile.bind(fs, filename, 'TODO')
+  ], function (err) {
+    // throw any unexpected errors
+    if (err && err !== true) {
+      done(err);
+    // or finish without complaint
+    } else {
+      done();
+    }
+  });
+}
+
+function write_page (filepath, done) {
+  // defaults
+  if (!done) {
+    done = filepath;
+    filepath = this.filepath;
+  }
+
+  // the filename is just the path with a file extension
+  var filename = filepath + '.md';
+  async.series([
+    // see if it exists, and thus
+    // whether we should create it
+    fs.exists.bind(fs, filename),
+    fs.writeFile.bind(fs, filename, 'TODO')
+  ], function (err) {
+    if (err && err !== true) {
+      done(err);
+    } else {
+      done();
+    }
+  });
+}
+
+function create_docs (filepath, done) {
+  async.series([
+    fs.exists.bind(fs, filepath),
+    fs.mkdir.bind(fs, filepath)
+  ], function (err) {
+    if (err && err !== true) {
+      done(err);
+    } else {
+      done();
+    }
+  });
+}
+
+function create_docs_title (filepath, done) {
+  var filename = path.join(filepath, 'index.md');
+  async.series([
+    fs.exists.bind(fs, filename),
+    fs.writeFile.bind(fs, filename, 'TODO')
+  ], function (err) {
+    if (err && err !== true) {
+      done(err);
+    } else {
+      done();
+    }
+  });
+}
+
+var argv = process.argv.slice(2);
+var scaffold = new Scaffold({
+  filepath: argv[0] || 'docs',
+  sitemap_path: argv[1] || '../config.json'
+});
+
+async.series([
+  create_docs.bind(null, scaffold.filepath),
+  create_docs_title.bind(null, scaffold.filepath)
+], function (err) {
+  if (err) throw err;
+  scaffold.write(function (err) {
+    if (err) throw err;
+  });
+})
